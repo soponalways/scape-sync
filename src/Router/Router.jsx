d3d9e9299ac1380c6ffc69687ca51de40fdd422e
Sopon islam <soponalways@gmail.com>
2025-09-24 22:42:44 +0600

    Project Setup

---
+++ src/Router/Router.jsx
@@ -0,0 +1,8 @@
+import { createBrowserRouter } from "react-router";
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <h1 className="text-3xl font-bold underline">Rokeya + Sopon</h1>,
+    }
+])
