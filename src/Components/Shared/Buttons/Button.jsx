ecd5f059dab7fcb29e68b4c258114f9e17ca989b
Sopon islam <soponalways@gmail.com>
2025-09-25 11:00:59 +0600

    Button element Update

--- src/Components/Shared/Buttons/Button.jsx
+++ src/Components/Shared/Buttons/Button.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const Button = ({ label, onClick, className, variant = "primary", }) => {
+const Button = ({ label, onClick, className, variant = "primary" }) => {
     const classes = {
-        primary: 'bg-blue-500 text-white hover:bg-blue-600',
-        secondary: 'bg-gray-500 text-white hover:bg-gray-600',
+        primary: 'bg-primary text-white hover:bg-primary-focus',
+        outline: 'bg-transparent border border-primary text-primary hover:bg-primary hover:text-white',
         danger: 'bg-red-500 text-white hover:bg-red-600',
     };
     return (
-        <button onClick={onClick} className={` ${classes[variant]} ${className}`}>
+        <button onClick={onClick} className={`rounded-sm md:rounded-md lg:rounded-lg cursor-pointer hover:scale-105 transition-all transform duration-300 font-public-sans md:font-semibold font-medium lg:font-bold ${classes[variant]} ${className}`}>
             {label}
         </button>
     );
